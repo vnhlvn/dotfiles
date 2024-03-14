c683b84db6f1a917d1a799b9c9da3d1d4ed542a1
Ivan Holovin <ivan.holovin@hotmail.com>
2024-03-14 15:53:03 +0200

    feat(Neovim): Add vim and lsd aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,3 +1,11 @@
+vim() {
+    if [[ $# -eq 0 ]]; then
+        nvim .
+    else
+        nvim "$@"
+    fi
+}
+alias lsd="lsd -1X"
 alias vim="nvim"
 alias zshconfig="vim ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
