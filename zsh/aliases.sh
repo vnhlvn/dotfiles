267a713dc46d9877e869284328f46f6c1000482b
Ivan Holovin <ivan.holovin@hotmail.com>
2024-03-24 11:54:52 +0200

    fix(zsh): Remove redundant nvim alias

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -6,7 +6,6 @@ vim() {
     fi
 }
 alias lsd="lsd -1X"
-alias vim="nvim"
 alias zshconfig="vim ~/.zshrc"
 alias ohmyzsh="vim ~/.oh-my-zsh"
 alias fdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
