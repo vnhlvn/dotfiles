02ed9065d3b1bfeea53b1d7af981a55fe6ddcaed
Ivan Holovin <ivan.holovin@hotmail.com>
2024-04-30 11:25:26 +0300

    feat(nvim): Add fugitive shortcut

--- nvim/lua/custom/keymap.lua
+++ nvim/lua/custom/keymap.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pg", [[:G<CR>]])
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("n", "<leader><S-/>", [[:noh<CR>]])
