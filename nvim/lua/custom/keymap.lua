1305a05820ef873de187c4102b506ab5a251bc98
Ivan Holovin <ivan.holovin@hotmail.com>
2024-05-23 12:54:56 +0300

    chore(nvim): Add <leader> to LSP format keymap

--- nvim/lua/custom/keymap.lua
+++ nvim/lua/custom/keymap.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>")
 vim.keymap.set("n", "<leader>pg", [[:G<CR>]])
 vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("n", "ff", "<CMD>lua vim.lsp.buf.format()<CR>")
+vim.keymap.set("n", "<leader>ff", "<CMD>lua vim.lsp.buf.format()<CR>")
 
