c9d92511a7355a2c019ec363199d5576c18b2698
Ivan Holovin <ivan.holovin@hotmail.com>
2024-10-07 14:30:13 +0300

    feat(nvim): Add harpoon keymaps

--- nvim/lua/custom/keymap.lua
+++ nvim/lua/custom/keymap.lua
@@ -3,3 +3,6 @@ vim.keymap.set("n", "<leader>pg", [[:G<CR>]])
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("n", "<leader>ff", "<CMD>lua vim.lsp.buf.format()<CR>")
 
+vim.keymap.set('n', '<leader>ha', require('harpoon.mark').add_file)
+vim.keymap.set('n', '<leader>ht', require("harpoon.ui").toggle_quick_menu)
+
