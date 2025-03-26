a143182d7b3362a8d6723d68ccc1f51963f9e8d7
Ivan Holovin <ivan.holovin@hotmail.com>
2025-03-26 10:02:12 +0200

    feat(nvim): Add organize imports LSP mapping

--- nvim/init.lua
+++ nvim/init.lua
@@ -507,6 +507,15 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+
+  local organize_imports = function()
+    vim.lsp.buf.code_action({
+      context = { only = { "source.organizeImports" } },
+      apply = true -- Automatically apply without prompting
+    })
+  end
+
+  nmap('<leader>oi', organize_imports, '[O]rganize [I]mports')
 end
 
 -- document existing key chains
