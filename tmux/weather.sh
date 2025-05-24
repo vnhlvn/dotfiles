78ed2dd2f9827fdbb4b685b3445c42c5f08c9fd5
Ivan Holovin <ivan.holovin@hotmail.com>
2025-05-24 10:37:59 +0300

    Add day/night emojis in weather script

--- tmux/weather.sh
+++ tmux/weather.sh
@@ -18,28 +18,60 @@ if [ -z "$TEMP" ]; then
     exit 1
 fi
 
-case $WEATHER_CODE in
-    0) ICON="☀️" ;;
-    1|2|3) ICON="⛅" ;;
-    45|48) ICON="🌫️" ;;
-    51|53|55|56|57) ICON="🌦️" ;;
-    61|63|65|66|67) ICON="🌧️" ;;
-    71|73|75|77) ICON="❄️" ;;
-    80|81|82) ICON="🌦️" ;;
-    85|86) ICON="🌨️" ;;
-    95|96|99) ICON="⛈️" ;;
-    *) 
-        TEMP_INT=$(echo $TEMP | cut -d'.' -f1)
-        if [ "$TEMP_INT" -le 0 ]; then
-            ICON="❄️"
-        elif [ "$TEMP_INT" -le 15 ]; then
-            ICON="☁️"
-        elif [ "$TEMP_INT" -le 25 ]; then
-            ICON="⛅"
-        else
-            ICON="☀️"
-        fi
-        ;;
-esac
+HOUR=$(date +%H)
+IS_NIGHT=0
+if [ "$HOUR" -ge 20 ] || [ "$HOUR" -lt 6 ]; then
+    IS_NIGHT=1
+fi
+
+if [ "$IS_NIGHT" -eq 1 ]; then
+    case $WEATHER_CODE in
+        0) ICON="🌙" ;;
+        1|2|3) ICON="☁️" ;;
+        45|48) ICON="🌫️" ;;
+        51|53|55|56|57) ICON="🌧️" ;;
+        61|63|65|66|67) ICON="🌧️" ;;
+        71|73|75|77) ICON="❄️" ;;
+        80|81|82) ICON="🌧️" ;;
+        85|86) ICON="🌨️" ;;
+        95|96|99) ICON="⛈️" ;;
+        *) 
+            TEMP_INT=$(echo $TEMP | cut -d'.' -f1)
+            if [ "$TEMP_INT" -le 0 ]; then
+                ICON="❄️"
+            elif [ "$TEMP_INT" -le 15 ]; then
+                ICON="☁️"
+            elif [ "$TEMP_INT" -le 25 ]; then
+                ICON="☁️"
+            else
+                ICON="🌙"
+            fi
+            ;;
+    esac
+else
+    case $WEATHER_CODE in
+        0) ICON="☀️" ;;
+        1|2|3) ICON="⛅" ;;
+        45|48) ICON="🌫️" ;;
+        51|53|55|56|57) ICON="🌦️" ;;
+        61|63|65|66|67) ICON="🌧️" ;;
+        71|73|75|77) ICON="❄️" ;;
+        80|81|82) ICON="🌦️" ;;
+        85|86) ICON="🌨️" ;;
+        95|96|99) ICON="⛈️" ;;
+        *) 
+            TEMP_INT=$(echo $TEMP | cut -d'.' -f1)
+            if [ "$TEMP_INT" -le 0 ]; then
+                ICON="❄️"
+            elif [ "$TEMP_INT" -le 15 ]; then
+                ICON="☁️"
+            elif [ "$TEMP_INT" -le 25 ]; then
+                ICON="⛅"
+            else
+                ICON="☀️"
+            fi
+            ;;
+    esac
+fi
 
 echo -n "${ICON} ${TEMP}°C"
